1525f79cf401cbd3244a9c5274ce3f673cdbba79
Ritesh Kumar <ritesh.try.25@gmail.com>
2026-05-30 23:45:38 +0530

    connected production backend

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = "https://momentum-v11.onrender.com/api";
 
 export const formatDateKey = (date) => {
     const year = date.getFullYear();
